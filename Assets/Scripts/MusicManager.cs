6627241595c8fc9a6300570858923b069188dc69
yeonico <nicola.yeo@outlook.com>
2022-07-23 17:01:44 +1200

    Added music player

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] private bool musicIsOn;
+    [SerializeField] private AudioClip bgSong;
+    private AudioSource musicSource;
+
+    void Start()
+    {
+        musicSource = GetComponent<AudioSource>();
+        musicSource.clip = bgSong;
+
+        if (musicIsOn)
+        {
+            musicSource.Play();
+        }
+    }
+
+}
